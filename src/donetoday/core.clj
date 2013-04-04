c88c826a4f6f225f53ae1239c09d9055c4d2d1ab
Nathan McFarland <nathan.mcfarland@nmcfarl.org>
2013-04-04 08:55:02 -0700

    --report flag to report on the last month

--- src/donetoday/core.clj
+++ src/donetoday/core.clj
@@ -50,6 +50,7 @@
                                       (time/minus (local-now) (time/days 1)))))
 
 (defn- stringdate-to-integer [date] (if date (Integer. (apply str (filter #(Character/isDigit %) date))) nil))
+(defn- date->integer [date] (if date (Integer. (apply str (filter #(Character/isDigit %)  (unparse dynamodb-formatter date)))) nil))
 
 
 ;;DynamoDB stuff
@@ -93,10 +94,16 @@
 
 ;; actaul functionality
 
+(defn- gen-day
+  "Just show a days info"
+  [columnname view-date]
+      (pprint/cl-format nil "--------------------------------------~%I did this ~A: ~%~%~{* ~A~%~}~%" (dynamodb2display view-date) (dt-get-column columnname view-date)))
+
+
 (defn- view-day
   "Just show a days info"
   [columnname view-date]
-  (pprint/cl-format true "I did this ~A: ~%~%~{* ~A~%~}~%" (dynamodb2display view-date) (dt-get-column columnname view-date)))
+  (println (gen-day columnname view-date)))
 
 (defn- get-date-from-options
   "figure out the date from the options"
@@ -109,6 +116,19 @@
        default))))
                                      
 
+(defn- view-month
+  "runs view day for a month"
+  [columnname view-date]
+  (pprint/cl-format true "~{~A~}~%"
+      (map #(gen-day columnname
+                              (date->integer
+                               (time/minus 
+                                (parse dynamodb-formatter (str view-date))
+                                (time/days %))))
+           (reverse (range 1 31)))))     
+
+                                     
+                                     
 (defn- add-to-done-today
   "Add an item to today's list."
   [columnname thingsdone date]
@@ -123,16 +143,19 @@
   "Record and view done things."
   [& args]
   (let [[options thingsdone banner] (cli args
-                                         
+                                         ["-r" "--report" "Report things done" :flag true]
                                          ["-v" "--view" "View things done" :flag true]
                                          ["-d" "--date" (str "The date you wish to view eg: " (lastmonth))]
                                          ["-y" "--yesterday" "change the date to yesterday" :flag true]
                                          ["-t" "--type" "Type of thing being added" :default "done" ]
                                          ["-h" "--help" "Show help" :default false :flag true]                                         
                                          )]
-    (when (or (:help options) (not (or  (:view options) (first thingsdone))))
+    (when (or (:help options) (not (or  (:view options) (first thingsdone) (:report options))))
       (println banner)
       (System/exit 0))
+    (if (:report options)
+      (do (println "Monthly Report")
+      (view-month  (:type options) (get-date-from-options options (today)))))
     (if (:view options)
       (view-day (:type options) (get-date-from-options options (lastmonth))))
     (if (first thingsdone)
